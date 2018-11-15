9664880df2fe2e1fac3afe529c2fed26caff03e9
mpotts0201 <mpotts0201@gmail.com>
2018-11-15 11:22:12 -0500

    Model created and dependencies installed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+murphy = User.create(email: 'murphy@potts.com', password: 'password', password_confirmation: 'password')
+
+
+yoda = Creature.create(name: 'Yoda', description: 'Short and green', user_id: murphy.id)
+
+chewy = Creature.create(name: 'Chewy', description: 'Tall and hairy', user_id: murphy.id)
+
+r2d2 = Creature.create(name: 'R2', description: 'Short and shiny', user_id: murphy.id)
